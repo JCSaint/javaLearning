1443da01a15a2a6d70840948b7437130b361f7f3
JCSaint <jeancsantoscdc@gmail.com>
2023-02-02 18:18:55 -0300

    Iniciando capítulo 5, herança e polimorfismo

---
+++ livro-01-intro-java-poo/introJavaPOO/src/introJavaPOO/Livro.java
@@ -0,0 +1,87 @@
+package introJavaPOO;
+
+public class Livro {
+	private String nome;
+	private String descricao;
+	private double valor;
+	private String isbn;
+	private Autor autor;
+	
+	Livro(Autor autor){
+		this.autor = autor;
+	}
+
+	void adicionaValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	double retornaValor() {
+		return valor;
+	}
+
+	boolean aplicaDescontoDe(double porcentagem) {
+		if (porcentagem > .3) {
+			return false;
+		}
+		this.valor -= valor * porcentagem;
+		return true;
+	}
+
+	void mostrarDetalhes() {
+		System.out.println("Detalhes do livro");
+		System.out.println("Nome: " + nome);
+		System.out.println("Descrição: " + descricao);
+		System.out.println("Valor: " + valor);
+		System.out.println("ISBN: " + isbn);
+		System.out.println("--");
+
+		if (temAutor()) {
+			autor.mostrarDetalhes();
+		}
+	}
+
+	boolean temAutor() {
+		return this.autor != null;
+	}
+
+}
